b35f32bb72fde72c5ee97ab8154d99e76b65f522
ayumi.goto <ayumi.goto.1016@gmail.com>
2021-10-09 17:42:03 +0000

    laravel set

---
+++ resources/views/dice.blade.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="https://fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">    
+    <title>クトゥルフ</title>
+    <link rel="stylesheet" href="./style.css" />
+  </head>  
+<body>
+  <h1>クトゥルフ用ルーレット</h1>
+  <div id ="unit">
+  <div id ="box0">?</div>
+  <div>
+    <button type="button0" id="roulette0">1D100</button>
+  </div>
+  </div> 
+  <div id ="unit">
+  <div id ="box1_1">?</div>
+  <div>
+    <button type="button1_1" id="roulette1_1">1D2</button>
+  </div>
+  </div> 
+  <div id ="unit">
+  <div id ="box1_2">?</div>
+  <div>
+    <button type="button1_2" id="roulette1_2">1D3</button>
+  </div>
+  </div>   
+  <div id ="unit">
+  <div id ="box1_3">?</div>
+  <div>
+    <button type="button1_3" id="roulette1_3">1D4</button>
+  </div>
+  </div>    
+  <div id ="unit">
+  <div id ="box1_4">?</div>
+  <div>
+    <button type="button1_4" id="roulette1_4">1D6</button>
+  </div>
+  </div>
+  <div id ="unit">
+  <div id ="box1_5">?</div>
+  <div>
+    <button type="button1_5" id="roulette1_5">1D8</button>
+  </div>
+  </div>  
+  <div id ="unit">
+  <div id ="box1_6">?</div>
+  <div>
+    <button type="button1_6" id="roulette1_6">1D10</button>
+  </div>
+  </div> 
+  <div id ="unit">
+  <div id ="box1_7">?</div>
+  <div>
+    <button type="button1_7" id="roulette1_7">1D12</button>
+  </div>
+  </div>   
+  <div id ="unit">
+  <div id ="box1_8">?</div>
+  <div>
+    <button type="button1_8" id="roulette1_8">1D20</button>
+  </div>
+  </div>     
+  <div id ="unit">
+  <div id ="box2">?</div>
+  <div>
+    <button type="button2" id="roulette2">2D6</button>
+  </div>
+  </div>  
+  <div id ="unit">
+  <div id ="box3">?</div>
+  <div>
+    <button type="button3" id="roulette3">3D6</button>
+  </div>
+  </div>   
+</body>
+  <script src="main.js"></script>
+</html>
